c6b165bfd1ca8ae05bef0d51fdae754f9fdaa80d
Clayton Wakefield <acidduckling@gmail.com>
2018-04-29 14:14:01 +1000

    151 - Accessing Protected API Resources

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,25 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { AuthHttp } from 'angular2-jwt';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private http: Http) {
+  constructor(private authHttp: AuthHttp) {
   }
 
   getOrders() { 
-    return this.http.get('/api/orders')
+    // let headers = new Headers();
+    // let token = localStorage.getItem('token');
+    // headers.append('authorization', 'Bearer ' + token);
+
+    // let options = new RequestOptions({
+    //   headers: headers
+    // });
+    
+    // If we use AuthHttp, we do not need to use the above code to add the authorization header - its handled automatically for us.
+    return this.authHttp.get('/api/orders'/*, options*/)
       .map(response => response.json());
   }
 }
